2fe7853317eef518c6f94b7016d57271e1e7c5a0
tobexcode <tobexalox@gmail.com>
2024-01-19 12:41:49 +0100

    package test

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
-export {
-  Button,
-  buttonVariants,
-  type ButtonProps,
-} from "./components/Button/Button";
-export {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "./components/Avatar/Avatar";
-export { Alert, AlertDescription, AlertTitle } from "./components/Alert/Alert";
+export * from "./components/Button/Button";
+export * from "./components/Avatar/Avatar";
+export * from "./components/Alert/Alert";
 
 import "../style/styles.css";
