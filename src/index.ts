f29ffde4f2b02fbc43402b6b2b6b71bed65aa48e
tobexcode <tobexalox@gmail.com>
2024-01-19 15:55:00 +0100

    package test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { Button, buttonVariants, type ButtonProps } from "./components/Button";
-export { Avatar, AvatarFallback, AvatarImage } from "./components/Avatar";
-export { Alert, AlertDescription, AlertTitle } from "./components/Alert";
-
-import "../style/styles.css";
+export { Button, buttonVariants, type ButtonProps } from './components/Button';
+export { Avatar, AvatarFallback, AvatarImage } from './components/Avatar';
+export {Alert, AlertDescription, AlertTitle } from './components/Alert';
--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,29 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import { peerDependencies, dependencies } from "./package.json";
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
-import postcss from "rollup-plugin-postcss";
-import postcssImport from "postcss-import";
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { peerDependencies, dependencies } from './package.json'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: "classic",
+      'jsxRuntime': 'classic'
     }),
     dts({
-      include: ["src/**/*"],
-    }),
-    postcss({
-      extract: true, // Extract the CSS to a separate file
-      plugins: [postcssImport()],
-      modules: false, // Set to true if you want to enable CSS modules
-    }),
+      include: ['src/**/*'],
+    })
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, "src", "index.ts"),
-      formats: ["es", "cjs"],
+      entry: resolve(__dirname, 'src', 'index.ts'),
+      formats: ['es', 'cjs'],
       fileName: (ext) => `index.${ext}.js`,
     },
     rollupOptions: {
-      external: [
-        ...Object.keys(peerDependencies),
-        ...Object.keys(dependencies),
-      ],
-      output: { preserveModules: true, exports: "named" },
+      external: [...Object.keys(peerDependencies), ...Object.keys(dependencies)], output: { preserveModules: true, exports: 'named' }
     },
-
-    target: "esnext",
-    sourcemap: true,
-  },
-});
+    
+    target: 'esnext',
+    sourcemap: true
+  }
+})
