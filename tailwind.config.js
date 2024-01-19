eeb6548a8be888012163855cf11c70c994f1b08d
tobexcode <tobexalox@gmail.com>
2024-01-19 13:09:56 +0100

    package test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
+const { fontFamily } = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  safelist: ["dark"],
+  safelist: [
+    'dark'
+  ],
   theme: {
     container: {
       center: true,
@@ -74,4 +76,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+}
