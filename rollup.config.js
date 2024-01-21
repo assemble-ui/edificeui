b5bfd3172414770735472d1dff883ffbb68dadc1
tobexcode <tobexalox@gmail.com>
2024-01-21 17:38:18 +0100

    package test

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+import babel from "@rollup/plugin-babel";
+import external from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import { dts } from "rollup-plugin-dts";
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main, // Specify your CommonJS output file
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module, // Specify your ESM output file
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      postcss({
+        config: {
+          path: "./postcss.config.js",
+        },
+        extensions: [".css"],
+        minimize: true,
+        inject: {
+          insertAt: "top",
+        },
+      }),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"], // Add supported extensions
+        babelHelpers: "bundled", // Ensure bundled helpers are used
+      }),
+      external(),
+      resolve(),
+      terser(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.(css|less|scss)$/i], // Include file extensions
+  },
+];
