7ab824aa0b98394e23d07e19d8dbc9f3b147862f
tobexcode <tobexalox@gmail.com>
2024-01-24 12:50:43 +0100

    package test

--- src/declarations.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.jpg'
-declare module '*.png'
-declare module '*.svg'
-declare module '*.gif'
-declare module '*.jpeg'
-declare module '*.webp'
-declare module '*.mp4'
-declare module '*.mp3'
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { App } from './App'
-import { createRoot } from 'react-dom/client'
-import './globals.css'
-
-const container: HTMLElement | null = document.getElementById('root')
-const root = createRoot(container!)
-root.render(<App />)
